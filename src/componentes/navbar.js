1e5d944120827b87d917ed0dc05b5b2d43f8c91e
George Moreno <gmodeveloper@gmail.com>
2020-04-28 15:42:49 -0300

    Atividade 2

--- src/componentes/navbar.js
+++ src/componentes/navbar.js
@@ -6,10 +6,7 @@ class Navbar extends React.Component {
   constructor(props) {
     super(props);
     this.name = this.props.name;
-<<<<<<< HEAD
-=======
     // Recebendo por PROPS o nome que vem do App;
->>>>>>> 441de3051fb6506a0e46307c15899142a72c3cef
   }
   render() {
     return (
@@ -18,12 +15,9 @@ class Navbar extends React.Component {
         <button className="btn">Linha do Tempo</button>
         <button className="btn">Perfil</button>
         <span className="right">{this.name}</span>
-<<<<<<< HEAD
-=======
         {
           //Rederizando o nome que chega por PROPS
         }
->>>>>>> 441de3051fb6506a0e46307c15899142a72c3cef
       </div>
     );
   }
